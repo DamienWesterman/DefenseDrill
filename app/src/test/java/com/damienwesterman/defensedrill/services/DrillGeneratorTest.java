9f7ca76a9acd67cc2f844effa72fdb0195df31bf
Damien Westerman <damien.westerman@snhu.edu>
2024-05-22 20:07:48 -0600

    Initial commit

---
+++ app/src/test/java/com/damienwesterman/defensedrill/services/DrillGeneratorTest.java
@@ -0,0 +1,15 @@
+package com.damienwesterman.defensedrill.services;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DrillGeneratorTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+    // When testing the random generation, just make sure that it fits the criteria is all
+    // Then when re-generating drills, make sure that it isn't the same as the previously suggested one
+}
