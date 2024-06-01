04a0de756f31733fcb177a77db9eb83682e66148
Damien Westerman <damien.westerman@snhu.edu>
2024-06-01 17:43:59 -0600

    Adding util directory for clarity from Android Services

--- app/src/main/java/com/damienwesterman/defensedrill/utils/DrillGenerator.java
+++ app/src/main/java/com/damienwesterman/defensedrill/utils/DrillGenerator.java
@@ -9,7 +9,7 @@
  *                            *
  \****************************/
 
-package com.damienwesterman.defensedrill.services;
+package com.damienwesterman.defensedrill.utils;
 
 public class DrillGenerator {
 }
