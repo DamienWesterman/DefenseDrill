69519803bbe311986b4fff4c6c3fe6a26639b0ad
Damien Westerman <damien.westerman@snhu.edu>
2024-06-02 11:30:52 -0600

    Changing to use IDs in get methods

--- app/src/main/java/com/damienwesterman/defensedrill/database/DrillRepository.java
+++ app/src/main/java/com/damienwesterman/defensedrill/database/DrillRepository.java
@@ -75,41 +75,32 @@ public class DrillRepository {
     /**
      * Return a list of all Drills that belong to the specified group.
      *
-     * @param group Specific group of drills.
-     * @return      List of Drill objects.
+     * @param groupId   ID of the specific group of drills.
+     * @return          List of Drill objects.
      */
-    public synchronized List<Drill> getAllDrills(GroupEntity group) {
-        if (null == group) {
-            return new ArrayList<>();
-        }
-        return this.drillDao.findAllDrillsByGroup(group.getId());
+    public synchronized List<Drill> getAllDrillsByGroupId(long groupId) {
+        return this.drillDao.findAllDrillsByGroup(groupId);
     }
 
     /**
      * Return a list of all Drills that belong to the specified sub group.
      *
-     * @param subGroup  Specific sub group of drills.
-     * @return          List of Drill objects.
+     * @param subGroupId    ID of the specific sub group of drills.
+     * @return              List of Drill objects.
      */
-    public synchronized List<Drill> getAllDrills(SubGroupEntity subGroup) {
-        if (null == subGroup) {
-            return new ArrayList<>();
-        }
-        return this.drillDao.findAllDrillsBySubGroup(subGroup.getId());
+    public synchronized List<Drill> getAllDrillsBySubGroupId(long subGroupId) {
+        return this.drillDao.findAllDrillsBySubGroup(subGroupId);
     }
 
     /**
      * Return a list of all Drills that belong to both the specified group and sub group.
      *
-     * @param group     Specific group of drills.
-     * @param subGroup  Specific sub group of drills.
-     * @return          List of Drill objects.
+     * @param groupId       ID of the specific group of drills.
+     * @param subGroupId    ID of the specific sub group of drills.
+     * @return              List of Drill objects.
      */
-    public synchronized List<Drill> getAllDrills(GroupEntity group, SubGroupEntity subGroup) {
-        if (null == group || null == subGroup) {
-            return new ArrayList<>();
-        }
-        return this.drillDao.findAllDrillsByGroupAndSubGroup(group.getId(), subGroup.getId());
+    public synchronized List<Drill> getAllDrills(long groupId, long subGroupId) {
+        return this.drillDao.findAllDrillsByGroupAndSubGroup(groupId, subGroupId);
     }
 
     /**
@@ -427,14 +418,11 @@ public class DrillRepository {
     /**
      * Get all subGroups of a given group.
      *
-     * @param group Group to search for its subGroups.
-     * @return      List of SubGroupEntity objects.
+     * @param groupId   ID of the group to search for its subGroups.
+     * @return          List of SubGroupEntity objects.
      */
-    public synchronized List<SubGroupEntity> getAllSubGroups(GroupEntity group) {
-        if (null == group) {
-            return new ArrayList<>();
-        }
-        return this.subGroupDao.findAllByGroup(group.getId());
+    public synchronized List<SubGroupEntity> getAllSubGroups(long groupId) {
+        return this.subGroupDao.findAllByGroup(groupId);
     }
 
     /**
