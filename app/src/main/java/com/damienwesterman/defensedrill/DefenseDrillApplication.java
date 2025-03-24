130f7ae40aa0e61585e68bfd744cc3ce783a5663
Damien Westerman <damien.westerman@snhu.edu>
2025-03-24 10:52:00 -0600

    Dependencies for Hilt/Dagger

---
+++ app/src/main/java/com/damienwesterman/defensedrill/DefenseDrillApplication.java
@@ -0,0 +1,9 @@
+package com.damienwesterman.defensedrill;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class DefenseDrillApplication extends Application {
+}
